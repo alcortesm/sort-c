af0a2377c07209853e2dd1b76edc9f866c2ceb3a
Alberto Cortés <alberto@sourced.tech>
2016-07-15 23:19:04 +0200

    add simple bubble sort implementation

--- src/bubble.c
+++ src/bubble.c
@@ -1,5 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "util.h"
+
+void swap(int*a, int i, int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+}
+
 int* sort_bubble(int* a, int n) {
-    (void) n;
+    if (a == NULL || n == 0) {
+        return NULL;
+    }
+
+    if (n == 1) {
+        return a;
+    }
+
+    int limit;
+    for (limit=n; limit>1; limit--) {
+        int i;
+        for (i=1; i<limit; i++) {
+            char* s = to_str(a, n);
+            printf("i=%d limit=%d array=%s\n", i, limit, s);
+            free(s);
+
+            if (a[i] < a[i-1]) {
+                swap(a, i, i-1);
+                printf("    swap %d and %d\n", a[i], a[i-1]);
+            }
+        }
+    }
 
     return a;
 }
