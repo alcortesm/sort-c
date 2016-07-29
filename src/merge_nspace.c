93ed865e100b9a77c63cde09952e280a8bc2d198
Alberto Cortés <alberto@sourced.tech>
2016-07-29 02:27:25 +0200

    clean code

--- src/merge_nspace.c
+++ src/merge_nspace.c
@@ -6,7 +6,7 @@
 #include "array.h"
 
 void _sort_merge_nspace(int* dst, int* src, int f, int n);
-void merge(int* dst, int* src, int a, int b, int c, int d);
+void merge(int* dst, int* src, int a, int m, int d);
 
 int sort_merge_nspace(array* a) {
     int sz = array_size(a);
@@ -34,20 +34,30 @@ void _sort_merge_nspace(int* tmp, int* src, int b, int e) {
 
     _sort_merge_nspace(tmp, src, b, b+sz/2);
     _sort_merge_nspace(tmp, src, b+sz/2, e);
-    merge(tmp, src, b, b+sz/2, b+sz/2, e);
+    merge(tmp, src, b, b+sz/2, e);
 }
 
-void merge(int* tmp, int* src, int ab, int ae, int bb, int be) {
+// merge rearranges the ints in the src array, from b up to, but not
+// including, e.
+//
+// Think of the array as two imaginary queues, one from b to the middle
+// element and another from the middle element to e.
+//
+// The smallest int from the head of each queue is dequeued and queued
+// into a temporal array, until both queues are empty.  Then the
+// contents of the temporal array are copied back into the original
+// array.
+void merge(int* tmp, int* src, int ab, int m, int be) {
     int* r1 = src+ab;
-    int* r2 = src+bb;
+    int* r2 = src+m;
     int i = ab;
     for (;;) {
-        if (r1 >= src+ae) {
+        if (r1 >= src+m) {
             memcpy(tmp+i, r2, (src+be-r2)*sizeof(int));
             break;
         }
         if (r2 >= src+be) {
-            memcpy(tmp+i, r1, (src+ae-r1)*sizeof(int));
+            memcpy(tmp+i, r1, (src+m-r1)*sizeof(int));
             break;
         }
         if (*r1 <= *r2) {
